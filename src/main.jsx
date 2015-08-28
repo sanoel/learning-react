074a87e0bb60d37f82289becd8e4eb4ceb850ef7
Samuel Adam Noel <Samuel@pal-nat184-174-054.itap.purdue.edu>
2015-08-28 14:14:48 -0400

    fixed a merge conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,3 @@
 var React = require('react');
 var App = require('./App.jsx');
-<<<<<<< HEAD
 React.render(<App />,document.getElementById('app-container'));
-=======
-
-React.render(<App />, document.getElementById('app-container'));
-
->>>>>>> 5e98033c751f92bf6da54778c4a48f2251e3a1fc
